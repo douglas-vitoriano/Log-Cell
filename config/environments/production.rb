804502e2f960bb3a24e33e5985380c02361a7334
Douglas <dvitoriano89@gmail.com>
2026-04-22 09:58:32 -0300

    fix: production config for railway

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,32 +1,107 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.enable_reloading = false
+  # Settings specified here will take precedence over those in config/application.rb.
+
+  # Code is not reloaded between requests.
+  config.cache_classes = true
+
+  # Eager load code on boot. This eager loads most of Rails and
+  # your application in memory, allowing both threaded web servers
+  # and those relying on copy on write to perform better.
+  # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
+
+  # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local = false
-  config.action_controller.perform_caching = true
-  config.public_file_server.headers = { "cache-control" => "public, max-age=#{1.year.to_i}" }
 
-  config.active_storage.service = :local
+  # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
+  # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
+  config.require_master_key = false
 
-  config.assume_ssl = true
-  config.force_ssl = true
+  # Disable serving static files from the `/public` folder by default since
+  # Apache or NGINX already handles this.
+  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
 
+  # Enable serving of images, stylesheets, and JavaScripts from an asset server.
+  # config.asset_host = "http://assets.example.com"
+
+  # Specifies the header that your server uses for sending files.
+  # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
+  # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
+
+  # Store uploaded files on the local file system (see config/storage.yml for options).
+  config.active_storage.service = :amazon
+
+  # Mount Action Cable outside main process or domain.
+  # config.action_cable.mount_path = nil
+  # config.action_cable.url = "wss://example.com/cable"
+  # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
+
+  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
+  # config.force_ssl = true
+
+  # Include generic and useful information about system operation, but avoid logging too much
+  # information to avoid inadvertent exposure of personally identifiable information (PII).
+  config.log_level = :info
+
+  # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
-  config.logger   = ActiveSupport::TaggedLogging.logger(STDOUT)
-  config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
 
-  config.silence_healthcheck_path = "/up"
+  config.lograge.enabled = true
+  config.lograge.ignore_actions = [ Healthcheck::CONTROLLER_ACTION ]
+
+  # Use a different cache store in production.
+  # config.cache_store = :mem_cache_store
+
+  # Use a real queuing backend for Active Job (and separate queues per environment).
+  # config.active_job.queue_adapter     = :resque
+  # config.active_job.queue_name_prefix = "backoffice_production"
+
+  config.action_mailer.perform_caching = false
+
+  # host autorizado
+  config.hosts << ENV["ALLOWED_HOST"]
+
+  allow_another_hosts = ENV["HEALTH_CHECKER_ALLOWED_IPS"] || ""
+  unless allow_another_hosts.empty?
+    ENV["HEALTH_CHECKER_ALLOWED_IPS"].split.select { |x| config.hosts << x.strip }
+  end
+
+  # Ignore bad email addresses and do not raise email delivery errors.
+  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
+  # config.action_mailer.raise_delivery_errors = false
+
+  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
+  # the I18n.default_locale when a translation cannot be found).
+  config.i18n.fallbacks = true
+
+  # Don't log any deprecations.
   config.active_support.report_deprecations = false
 
-  config.active_job.queue_adapter = :solid_queue
+  # Use default logging formatter so that PID and timestamp are not suppressed.
+  config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { host: "log-cell.up.railway.app" }
+  # Use a different logger for distributed setups.
+  # require "syslog/logger"
+  # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
 
-  config.i18n.fallbacks = true
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
+  end
+
+  config.host_authorization = {
+    exclude: ->(request) { request.path.include?("health_check") }
+  }
+
+  # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.active_record.attributes_for_inspect = [ :id ]
 
-  # Desabilita verificação de host — Railway usa proxy reverso
-  config.host_authorization = { exclude: ->(request) { true } }
+  # Use local timezone to timestamps over the application.
+  config.time_zone = "Brasilia"
+  config.active_record.default_timezone = :local
+
+  config.assets.compile = true
 end
